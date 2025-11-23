b650c5444961a86aadc6d559a0d050fe67acc1da
KJW <rlawjddns0@naver.com>
2025-11-23 19:48:47 +0900

    sns project init

---
+++ src/main/java/com/pastcampus/sns/SnsApplication.java
@@ -0,0 +1,14 @@
+package com.pastcampus.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pastcampus/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pastcampus.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
